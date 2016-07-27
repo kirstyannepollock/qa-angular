d978c28cc83b0ea21129d3c28a92518ccb502ff6
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-27 12:22:32 +0200

    Builds and can show basic page

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var sassSrc = './scss/**/*.scss';
+var cssDest = './css';
+var jQuerySrc = './bower_components/jquery/dist/**/*.js';
+var jQueryDest = './js/jquery/dist' ;
+
 var onError = function(err) {
-    console.log(err);
+  console.log(err);
 };
 
 gulp.task('sass', function () {
-  gulp.src('./scss/**/*.scss')
+  gulp.src(sassSrc)
     .pipe(sass({includePaths: ['./scss']}).on('error', sass.logError))
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest(cssDest));
 });
 
 gulp.task( 'jquery', function() {
-  return  gulp.src('./bower_components/jquery/dist/**/*.js')
-            .pipe(gulp.dest( './js/jquery/dist' ) );
+  return  gulp.src(jQuerySrc)
+            .pipe(gulp.dest(jQueryDest) );
 });
 
 // gulp.task('default', function () {
 //   gulp.watch('./scss/**/*.scss', ['sass']);
 // });
+
+//**TODO: add***
+
+// gulp.task( 'automate', function() {
+//   gulp.watch( [ sassSrc, jsSrc ], [ 'scripts', 'styles' ] );
+// });
+ 
+// gulp.task( 'default', ['scripts', 'styles', 'automate'] );
