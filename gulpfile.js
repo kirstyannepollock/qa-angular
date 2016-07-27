28e71615f76739ec1fde2ecc17fdeccd564bc16b
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-27 12:11:16 +0200

    Added jQuery via bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var onError = function(err) {
+    console.log(err);
+};
+
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass({includePaths: ['./scss']}).on('error', sass.logError))
     .pipe(gulp.dest('./css/'));
+});
+
+gulp.task( 'jquery', function() {
+  return  gulp.src('./bower_components/jquery/dist/**/*.js')
+            .pipe(gulp.dest( './js/jquery/dist' ) );
 });
 
 // gulp.task('default', function () {
